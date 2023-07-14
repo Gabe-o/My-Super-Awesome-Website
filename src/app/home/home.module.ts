6703197efa00992939b6123a31982a507d5ee2e2
Gabriel <gabe.olivotto@gmail.com>
2023-07-13 20:45:24 -0400

    made display more responsive to different devices

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProjectCardComponent } from './project-card/project-card.component';
 
 const components = [HomeComponent];
 
 @NgModule({
-  declarations: [...components],
+  declarations: [...components, ProjectCardComponent],
   imports: [CommonModule, SharedModule],
   exports: [...components],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,9 +6,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { GameOfLifeCanvasComponent } from './game-of-life-canvas/game-of-life-canvas.component';
 
-const modules = [RouterModule, MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule];
+const modules = [RouterModule, MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatGridListModule];
 
 @NgModule({
   declarations: [GameOfLifeCanvasComponent],
