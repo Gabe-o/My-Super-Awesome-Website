22471dc2006026f4b3f4aa0225c29090f9016a0e
Gabriel <gabe.olivotto@gmail.com>
2023-07-15 17:11:49 -0400

    removed topbar added footer instead

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProjectCardComponent } from './project-card/project-card.component';
+import { FooterComponent } from './footer/footer.component';
 
 const components = [HomeComponent];
 
 @NgModule({
-  declarations: [...components, ProjectCardComponent],
+  declarations: [...components, ProjectCardComponent, FooterComponent],
   imports: [CommonModule, SharedModule],
   exports: [...components],
 })
